1cc2f3ab8e91a27b1718cadf42417b44ea8c7253
Beth Tenorio <betsky98@gmail.com>
2013-09-08 20:02:31 -0400

    Add method to sanitize numeric data prior to seeding

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,23 +10,27 @@ require 'CSV'
 
 all_records = {}
 
+def sanitize(item)
+  item.gsub(/[$, ]/, '') if !item.nil?
+end
+
 CSV.foreach('db/data/mass_chip_data.csv', headers: true) do |row|
   all_records[row[0]] = {
     :geography => row['Geography'],
-    :total_pop_year_2005 => row['total pop, year 2005'].to_i,
-    :age_0_9_year_2005 => row['age 0-19, year 2005'].to_i,
-    :age_65_above_year_2005 => row['age 65+, year 2005'].to_i,
-    :per_capita_income_year_2000 => row['Per Capita Income, year 2000'].to_i,
-    :persons_living_below_200_percent_poverty_2000 => row['Persons Living Below 200% Poverty, year 2000'].to_i,
-    :percent_all_persons_living_below_200_percent_poverty_level_2000 => row['% all Persons Living Below 200% Poverty Level, year 2000'].to_f,
-    :percent_adequacy_prenatal_care__kotelchuck_ => row['% adequacy prenatal care (kotelchuck)'].to_f,
-    :percent_c_section_deliveries_2005_2008 => row['% C-section deliveries, 2005-2008'].to_f,
-    :number_of_infant_deaths_2005_2008 => row['Number of infant deaths, 2005-2008'].to_f,
-    :infant_mortality_rate__deaths_per_1000_live_births__2005_2008 => row['Infant mortality rate (deaths per 1000 live births), 2005-2008'].to_f,
-    :percent_low_birthweight_2005_2008 => row['% low birthweight 2005-2008'].to_f.to_f,
-    :percent_multiple_births_2005_2008 => row['% multiple births, 2005-2008'].to_f,
-    :percent_publicly_financed_prenatal_care_2005_2008 => row['% publicly financed prenatal care, 2005-2008'].to_f,
-    :percent_teen_births_2005_2008 => row['% teen births, 2005-2008'].to_f
+    :total_pop_year_2005 => sanitize(row['total pop, year 2005']).to_i,
+    :age_0_9_year_2005 => sanitize(row['age 0-19, year 2005']).to_i,
+    :age_65_above_year_2005 => sanitize(row['age 65+, year 2005']).to_i,
+    :per_capita_income_year_2000 => sanitize(row['Per Capita Income, year 2000']).to_i,
+    :persons_living_below_200_percent_poverty_2000 => sanitize(row['Persons Living Below 200% Poverty, year 2000']).to_i,
+    :percent_all_persons_living_below_200_percent_poverty_level_2000 => sanitize(row['% all Persons Living Below 200% Poverty Level, year 2000']).to_f,
+    :percent_adequacy_prenatal_care__kotelchuck_ => sanitize(row['% adequacy prenatal care (kotelchuck)']).to_f,
+    :percent_c_section_deliveries_2005_2008 => sanitize(row['% C-section deliveries, 2005-2008']).to_f,
+    :number_of_infant_deaths_2005_2008 => sanitize(row['Number of infant deaths, 2005-2008']).to_f,
+    :infant_mortality_rate__deaths_per_1000_live_births__2005_2008 => sanitize(row['Infant mortality rate (deaths per 1000 live births), 2005-2008']).to_f,
+    :percent_low_birthweight_2005_2008 => sanitize(row['% low birthweight 2005-2008']).to_f,
+    :percent_multiple_births_2005_2008 => sanitize(row['% multiple births, 2005-2008']).to_f,
+    :percent_publicly_financed_prenatal_care_2005_2008 => sanitize(row['% publicly financed prenatal care, 2005-2008']).to_f,
+    :percent_teen_births_2005_2008 => sanitize(row['% teen births, 2005-2008']).to_f
   }
 end
 
